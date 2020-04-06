d650ad58deaa52c0d9cad60554be87f1fe1406cb
Sejal Dua <sejal.dua@tufts.edu>
2020-04-06 14:08:12 -0700

    styling

--- hw10/script.js
+++ hw10/script.js
@@ -21,12 +21,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 var data = JSON.parse(request.responseText).meals;
                 s = "<br><br><table class='table'>";
                 s += "<tr class='hrow'>";
-                s += "<th><b>Meal Title</b></th>";
+                s += "<th id='col1'><b>Meal Title</b></th>";
                 s += "<th><b>Image</b></th>";
                 s += "</tr>";
                 for (x in data) {
                     s += "<tr>";
-                    s += "<th>" + data[x].strMeal + "</th>";
+                    s += "<th id='col1'>" + data[x].strMeal + "</th>";
                     s += "<th><img src='" + data[x].strMealThumb + "' width='100' height='100'></img></th>";
                     s += "</tr>";
                 }

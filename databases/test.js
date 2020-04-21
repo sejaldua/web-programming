87124915dc80ad56c599a47713622a9d40b59e1d
Sejal Dua <sejal.dua@tufts.edu>
2020-04-21 02:54:25 -0700

    first step of database hw

---
+++ databases/test.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+const fastcsv = require("fast-csv");
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://sejaldua:@cluster0-eeltb.mongodb.net/test?retryWrites=true&w=majority";
+
+function main() 
+{
+    let stream = fs.createReadStream("companies.csv");
+    let csvData = [];
+    let csvStream = fastcsv
+    .parse()
+    .on("data", function(data) {
+        csvData.push({
+        company: data[0],
+        ticker: data[1]
+        });
+    })
+    .on("end", function() {
+        // remove the first line: header
+        csvData.shift();
+
+        console.log(csvData);
+
+        MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, native_parser:true}, function(err, db) {
+            if (db) {
+                if(err) { return console.log(err); }
+            
+                var dbo = db.db("companies");
+                var collection = dbo.collection('stocks');
+                
+                console.log("here");
+                collection.insertMany(csvData, (err, res) => {
+                    if (err) throw err;
+                    console.log(`Inserted: ${res.insertedCount} rows`);
+                    db.close();
+                });
+            }
+            else {
+                console.log("oops");
+            }
+            console.log("Success!");
+        });
+    });
+    stream.pipe(csvStream);
+}
+
+main();
